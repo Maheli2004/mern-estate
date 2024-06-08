2a34d9b2df6d255e7d666888930286e2860593ab
Maheli2004 <maheli.20211110@iit.ac.lk>
2024-06-08 09:32:45 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
 
             <Link to="/">
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <img src="https://res.cloudinary.com/dv0m2dtyw/image/upload/v1717816792/guy1nnk4w7tn5jhzj19v.png" className="h-8" alt="Flowbite Logo" />
                 <span className='text-slate-500'>Nest</span>
                 <span className='text-slate-200'>Easy</span>
             </h1>
@@ -19,21 +20,16 @@ export default function Header() {
                 <Link to="/sign-in"><li className="text-gray-300 transition-colors duration-300 hover:text-white font-semibold text-lg">Sign In</li></Link>
             </ul>
             
-            <form className='p-2.5 rounded-lg flex items-center m-1 text-sm border border-gray-400 backdrop-brightness-40'>
+            <form className='p-2.5 rounded-lg flex items-center m-1 text-sm border border-gray-400 backdrop-brightness-40 hover:border-white'>
                 <input type='text' placeholder='Search...' 
                 className='text-white bg-transparent focus:outline-none w-24 sm:w-64'/>
 
                 <FaSearch className="text-white"/>
                 {/* <FaSearch className="text-slate-600"/> */}
-                
+
             </form>
     
         </div>
     </header>
   )
 }
-
-
-
-
-
