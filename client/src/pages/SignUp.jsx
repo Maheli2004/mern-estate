5d906d5a2eca3ca39bc77f6f8965609f41b82aca
Maheli2004 <maheli.20211110@iit.ac.lk>
2024-06-09 14:16:33 +0530

    Complete sign-up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,50 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {FaSearch} from "react-icons/fa";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        console.log(data);
+        if(data.success === false) {
+          setLoading(false);
+          setError(data.message);
+          return;
+        }
+        setLoading(false);
+        setError(null);
+        navigate("/sign-in");
+        // console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  // console.log(formData);
+  
   return (
     <div className="py-16">
       <div className="flex bg-white rounded-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl" style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)" }}>
@@ -16,7 +59,7 @@ export default function SignUp() {
           </div>
 
           <div className="absolute bottom-10 left-10 right-30 bg-black bg-opacity-75 text-white text-center py-1 rounded-2xl">
-            <button className="inline-flex items-center justify-center px-4 py-2 bg-transparent border border-transparent font-semibold text-white">
+            <button className="inline-flex items-center justify-center px-4 py-2 bg-transparent border border-transparent font-semibold text-white ">
               <FaSearch className="h-5 w-6 mr-2 text-white" />
               Watch Demo
             </button>
@@ -28,11 +71,14 @@ export default function SignUp() {
 
           <h2 className="text-3xl font-bold text-gray-700 text-center">SIgn up</h2>
 
-          <form className="flex flex-col mt-6">
-            <input type="text" placeholder="Username" className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:border-[#3182CE] mt-2" id="username" />
-            <input type="email" placeholder="Email" className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:border-[#3182CE] mt-4" id="email" />
-            <input type="password" placeholder="Password" className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:border-[#3182CE] mt-4" id="password" />
-            <button className="bg-[#3182CE] rounded-lg text-white shadow-lg py-3 mt-6 hover:bg-blue-600 transition duration-300">Sign Up</button>
+          <form onSubmit={handleSubmit} className="flex flex-col mt-6">
+            <input type="text" placeholder="Username" className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:border-[#3182CE] mt-2" id="username" onChange={handleChange}/>
+            <input type="email" placeholder="Email" className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:border-[#3182CE] mt-4" id="email" onChange={handleChange}/>
+            <input type="password" placeholder="Password" className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:border-[#3182CE] mt-4" id="password" onChange={handleChange}/>
+            {error && <p className="text-red-400 mt-0">{error}</p>}
+            <button disabled={loading} className="bg-[#3182CE] rounded-lg text-white shadow-lg py-3 mt-5 hover:bg-blue-600 transition duration-300 disabled:opacity-80">
+              {loading ? "Loading..." : "Sign Up"}
+            </button>
           </form>
 
           <p className="text-gray-600 mt-2 text-center sm:text-left">Already a member? <Link to={"/sign-in"} className="text-[#3182CE] hover:underline">Sign in</Link></p>
