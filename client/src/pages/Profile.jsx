137985f7c73a55aab3e0bf4ad42ae0345e4b2689
Maheli2004 <maheli.20211110@iit.ac.lk>
2024-06-12 11:35:48 +0530

    complete show listings fuctionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { FaArrowLeft } from 'react-icons/fa';
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
@@ -14,6 +15,8 @@ export default function Profile() {
   const [fileUploadError,setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   // firebase storage
@@ -111,6 +114,22 @@ export default function Profile() {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+
+      if (data.success === false) {
+        showListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      showListingsError(true)
+    }
+  };
+
   return (
     <div className ="max-w-3xl mx-4 sm:max-w-lg md:max-w-2xl lg:max-w-lg xl:max-w-3xl sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto bg-white shadow-xl rounded-lg p-3 mt-10 mb-10 ">
       <h1 className="text-3xl font-semibold text-center my-5">Profile</h1>
@@ -166,6 +185,34 @@ export default function Profile() {
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">Show Listing</button>
+      <p className="text-red-700 mt-5">{showListingsError ? 'Error showing listings' : ''}</p>
+
+      {userListings && 
+      userListings.length > 0 && 
+      <div className="flex flex-col gap-4">
+        <h1 className='text-center text-2xl font-semibold'> Your Listings</h1>
+        {userListings.map((listing) => ( 
+      <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+        
+        <Link  to={`/listing/${listing._id}`}>
+        <img src={listing.imageUrls} alt="listing cover" className="h-16 w-16 object-contain"/>
+        </Link>
+
+        <Link className='flex-1 text-slate-700 font-semibold hover:underline truncate' to={`/listing/${listing._id}`}>
+        <p>{listing.name}</p>
+        </Link>
+
+        <div className='flex flex-col items-center'>
+          <button className='text-red-700 uppercase'>Delete</button>
+          <button className='text-green-700 uppercase'>Edit</button>
+          <FaArrowLeft className="text-xl m-3" type='button' />
+          <FaArrowLeft className="text-xl m-3" type='button' />
+        </div>
+
+      </div>))}
+
+      </div>}
     </div>
-  )
+  );
 }
